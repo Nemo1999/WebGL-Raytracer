d19015997c85bbf027e930721876066d9bb38bda
nemo1999 <tiny.nemo.fish@gmail.com>
2020-12-02 10:50:04 +0800

    define findColor function

--- index.js
+++ index.js
@@ -9,13 +9,21 @@ function getGL(){
     return gl;
 }
 
-async function tracerMain(){
-    
-    var tracerVertexSource = await fetch("tracer_vert.glsl").then(r => r.text());
-    var tracerFragSource = await fetch("tracer_frag.glsl").then(r => r.text());
 
+
+async function tracerMain(){
+    //fetch shader source
+    var fetch_vert = fetch("tracer_vert.glsl").then(r => r.text());
+    var fetch_frag = fetch("tracer_frag.glsl").then(r => r.text());
+
+    //get WebGL context
     const gl = getGL();
-    
+
+    //get shader source 
+    var tracerVertexSource = await fetch_vert; 
+    var tracerFragSource = await fetch_frag;
+
+    // compile and link shader source 
     const tracerProgram = initShaderProgram(gl, tracerVertexSource, tracerFragSource);
     const tracerInfo = {
 	program: tracerProgram,
@@ -27,10 +35,76 @@ async function tracerMain(){
 	    ray00: gl.getUniformLocation(tracerProgram, 'ray00'),
 	    ray01: gl.getUniformLocation(tracerProgram, 'ray01'),
 	    ray10: gl.getUniformLocation(tracerProgram, 'ray10'),
-	    ray11: gl.getUniformLocation(tracerProgram, 'ray11')
-	    
+	    ray11: gl.getUniformLocation(tracerProgram, 'ray11'),
+	    sphereCenterRadius: gl.getUniformLocation(tracerProgram, "sphereCenterRadius"),
+	    sphereMaterial: gl.getUniformLocation(tracerProgram, "sphereMaterial"),
+	    sphereColor: gl.getUniformLocation(tracerProgram, "sphereColor")
 	},
     };
+
+    eye = {
+	pos: vec3.fromValues(0.0,0.0,0.0),
+	center: vec3.fromValues(0.0,0.0,-1.0),
+	up: vec3.fromValues(0.0,1.0,0.0)
+    };
+
+    //set ray directions
+    const viewProjectionMatrix = getViewProjMat(gl,eye);
+    const r00 = getEyeRay(viewProjectionMatrix,eye,-1,-1);
+    const r01 = getEyeRay(viewProjectionMatrix,eye,-1,1);
+    const r10 = getEyeRay(viewProjectionMatrix,eye,1,-1);
+    const r11 = getEyeRay(viewProjectionMatrix,eye,1,1);
+
+    //set sphere position and radius
+    var sphereCenterRadius = [0.0,-10.0,10.0,0.0,0.0,0.0,5.0,1.0];
+    for(var i=0;i<8*4;i++){
+	sphereCenterRadius.push(Infinity);
+    }
+    
+    //set sphere color 
+    var sphereColor = [0.5,0.7,1.0,1.0,0.3,0.3];
+    for(var i=0;i<8*3;i++){
+	sphereCenterRadius.push(0.0);
+    }
+
+    var sphereMaterial = [Number.NEGATIVE_INFINITY,Number.NEGATIVE_INFINITY];
+    for(var i=0;i<8;i++){
+	sphereMaterial.push(0.0);
+    }
+    
+    //Tell WebGL to use the program when drawing
+    gl.useProgram(tracerInfo.program);
+
+    //bind vertex attribute to shader (simple square attribute with four corner)
+    enableSquareVAO(gl,tracerInfo);
+    
+    //Set the shader uniforms
+    gl.uniform3fv(tracerInfo.uniformLocations.eye,eye.pos);
+    gl.uniform3fv(tracerInfo.uniformLocations.ray00,r00);
+    gl.uniform3fv(tracerInfo.uniformLocations.ray01,r01);
+    gl.uniform3fv(tracerInfo.uniformLocations.ray10,r10);
+    gl.uniform3fv(tracerInfo.uniformLocations.ray11,r11);    
+    gl.uniform4fv(tracerInfo.uniformLocations.sphereCenterRadius,new Float32Array(sphereCenterRadius));
+    gl.uniform3fv(tracerInfo.uniformLocations.sphereColor,sphereColor);
+    gl.uniform1fv(tracerInfo.uniformLocations.sphereMaterial,sphereMaterial);    
+
+    //clear framebuffer
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0); // clear anyting
+    gl.enable(gl.DEPTH_TEST); // Enable Depth Testing 
+    gl.depthFunc(gl.LEQUAL);// Near things obscure far things    
+    gl.clear(gl.COLOR_BUFFER_BIT| gl.DEPTH_BUFFER_BIT);
+    {
+	const offset = 0;
+	const vertextCount =4;
+	gl.drawArrays(gl.TRIANGLE_STRIP,offset,vertextCount);
+    }
+    
+}
+
+
+tracerMain();
+function enableSquareVAO(gl, tracerInfo){
     //create a buffer for square's positions.
     const positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -43,7 +117,7 @@ async function tracerMain(){
     gl.bufferData(gl.ARRAY_BUFFER,
 		  new Float32Array(positions),
 		  gl.STATIC_DRAW);
- 
+    
     //Tell WebGL how to pull out the position from the position buffer to the
     //vertexPosition attribute
     {
@@ -66,86 +140,40 @@ async function tracerMain(){
 	    tracerInfo.attribLocations.vertexPosition);
     }
 
-    eye = {
-	pos: vec3.fromValues(0.0,0.0,0.0),
-	center: vec3.fromValues(0.0,0.0,-1.0),
-	up: vec3.fromValues(0.0,1.0,0.0)
-    };
-
-    const getViewProjMat = function (eye){
-	const fieldOfView = 45 * Math.PI / 180;   // in radians
-	const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-	const zNear = 0.1;
-	const zFar = 100.0;
-	const projectionMatrix = mat4.create();
-	mat4.perspective(projectionMatrix,
-			 fieldOfView,
-			 aspect,
-			 zNear,
-			 zFar);
-	const viewMatrix = mat4.create();
-	mat4.lookAt(viewMatrix,eye.pos,eye.center,eye.up);
-	console.log(viewMatrix);
-	const ans = mat4.create();
-	return mat4.multiply(ans,viewMatrix, projectionMatrix);
-    }
-    function getEyeRay(matrix,eye, x, y){
-	const p0 = vec4.create();
-	const inv = mat4.create();
-	mat4.invert(inv,matrix)
-	vec4.transformMat4(p0, vec4.fromValues(x, y, 0.0, 1.0),inv );
-	console.log(x,y,p0)
-	const p1 = vec3.create();
-	vec3.scale(p1, vec3.fromValues(p0[0],p0[1],p0[2]), (1.0/p0[3]));
-	const ans = vec3.create();
-	vec3.subtract(ans,p1,eye.pos);
-	return ans;
-    }
-    const viewProjectionMatrix = getViewProjMat(eye);
-    console.log(viewProjectionMatrix);
-    const r00 = getEyeRay(viewProjectionMatrix,eye,-1,-1);
-    const r01 = getEyeRay(viewProjectionMatrix,eye,-1,1);
-    const r10 = getEyeRay(viewProjectionMatrix,eye,1,-1);
-    const r11 = getEyeRay(viewProjectionMatrix,eye,1,1);
-    console.log(r00,r01,r10,r11);
-    //Tell WebGL to use the program when drawing
-    gl.useProgram(tracerInfo.program);
-    
-    //Set the shader uniforms
-    gl.uniform3fv(
-	tracerInfo.uniformLocations.eye,
-	eye.pos);
-    //Set the shader uniforms
-    gl.uniform3fv(
-	tracerInfo.uniformLocations.ray00,
-	r00);
-    //Set the shader uniforms
-    gl.uniform3fv(
-	tracerInfo.uniformLocations.ray01,
-	r01);
-    gl.uniform3fv(
-	tracerInfo.uniformLocations.ray10,
-	r10);
-    gl.uniform3fv(
-	tracerInfo.uniformLocations.ray11,
-	r11);
-    
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clearDepth(1.0); // clear anyting
-    gl.enable(gl.DEPTH_TEST); // Enable Depth Testing 
-    gl.depthFunc(gl.LEQUAL);// Near things obscure far things    
-    gl.clear(gl.COLOR_BUFFER_BIT| gl.DEPTH_BUFFER_BIT);
-    {
-	const offset = 0;
-	const vertextCount =4;
-	gl.drawArrays(gl.TRIANGLE_STRIP,offset,vertextCount);
-    }
-    
 }
 
 
-tracerMain();
 
+const getViewProjMat = function (gl, eye){
+    const fieldOfView = 45 * Math.PI / 180;   // in radians
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+    const projectionMatrix = mat4.create();
+    mat4.perspective(projectionMatrix,
+		     fieldOfView,
+		     aspect,
+		     zNear,
+		     zFar);
+    const viewMatrix = mat4.create();
+    mat4.lookAt(viewMatrix,eye.pos,eye.center,eye.up);
+    console.log(viewMatrix);
+    const ans = mat4.create();
+    return mat4.multiply(ans,viewMatrix, projectionMatrix);
+}
+
+function getEyeRay(matrix,eye, x, y){
+    const p0 = vec4.create();
+    const inv = mat4.create();
+    mat4.invert(inv,matrix)
+    vec4.transformMat4(p0, vec4.fromValues(x, y, -1.0, 1.0),inv );
+    console.log(x,y,p0)
+    const p1 = vec3.create();
+    vec3.scale(p1, vec3.fromValues(p0[0],p0[1],p0[2]), (1.0/p0[3]));
+    const ans = vec3.create();
+    vec3.subtract(ans,p1,eye.pos);
+    return ans;
+}
 
 function initShaderProgram(gl,vsSource,fsSource){
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
--- tracer_frag.glsl
+++ tracer_frag.glsl
@@ -1,13 +1,90 @@
 // start of fragment shader
+#define numSphere 10
+#define maxDepth 3
 precision highp float;
 uniform vec3 eye;
 varying vec3 initialRay;
 //uniform float textureWeight;
 //uniform float timeSinceStart;
 //uniform sampler2D texture;
-//uniform float glossiness;
+
+//light position
+uniform vec3 lightPos;
+
+// record the position of the spheres
+// value of infinity  means the sphere should not be shown
+uniform vec4 sphereCenterRadius[numSphere];
+
+// record the material of the sphere
+// -inf for diffuse material
+// -k for glass with refractive constant k (k>0)
+//  0 for mirror material  
+//  g for glossiness of mirror material (g = random noise magnitude added to the reflected ray)
+uniform float sphereMaterial[numSphere];
+//record the color of each sphere
+uniform vec3 sphereColor[numSphere];
+
+const float inf = 1000000000000000.0;
+
+
+// find the intersection index of the given ray
+// t = inf means no intersection in the given direction
+float intersectSphere(vec3 origin, vec3 dir, vec3 center, float radius){
+  // length( (origin + t * dir) - center ) = radius
+  // to solve t, square both sides and solve the quadratic formula
+  float a = dot(dir,dir);
+  float b = 2.0 * dot(dir,origin - center);
+  float c = dot(origin - center,origin - center) - radius * radius;
+  float discriminate = b*b - 4.0 * a * c;
+  if(discriminate < 0.0) return inf;
+  float tNear = (-b - sqrt(discriminate))/ (2.0 * a);
+  if(tNear > 0.0 ) return tNear;
+  float tFar = (-b + sqrt(discriminate))/(2.0 * a);
+  if(tFar > 0.0) return tFar;
+  return inf;
+}
+
+// find intersection with multiple objects, return hitObjIndex and t 
+float intersectObjects(vec3 origin, vec3 dir, inout int hitObjIndex){
+  float tMin = inf;
+    for(int j=0;j < numSphere;j++){
+      //infinity radius means the sphere should not be drawn
+      if(sphereCenterRadius[j].w > inf)continue;
+      float t = intersectSphere(origin, dir, sphereCenterRadius[j].xyz,sphereCenterRadius[j].w);
+      if(t < tMin) {tMin = t; hitObjIndex = j;}
+    }
+    return tMin;
+}
+
+//
+//vec3 materialDiffuse(){;}
+
+vec3 findBackGround(vec3 origin, vec3 dir){
+  return mix(vec3(1.0,1.0,1.0),vec3(0.5,0.7,1.0),(dir.y+1.0)*0.5);
+}
+
+
+//find pixel color iteratively
+vec3 findColor(vec3  origin,vec3 dir ){
+  vec3 o = origin;
+  vec3 d = dir;
+  vec3 colorMask = vec3(1.0,1.0,1.0);
+  vec3 accumColor = vec3(0.0,0.0,0.0);
+  for(int i=0;i < maxDepth ;i++){
+    int hitObjIndex;
+    //record the nearest hit so far
+    float t = intersectObjects(o,d,hitObjIndex);
+    
+    
+  }
+  return accumColor;
+}
 
 void main(){
-  gl_FragColor = vec4(mix(vec3(1.0,1.0,1.0),vec3(0.0,0.0,1.0),(initialRay.y+1.0)*0.5) , 1.0);
+  float t = intersectSphere(eye, initialRay, vec3(0.0,0.0,-9.0), 2.0);
+  if(  t != inf )
+    gl_FragColor = vec4(1.0, 0.0, 0.0,1.0);//vec4(vec3((atan(t)/3.1415)+0.5),1.0);
+  else
+    gl_FragColor = vec4(mix(vec3(1.0,1.0,1.0),vec3(0.5,0.7,1.0),(initialRay.y+1.0)*0.5) , 1.0);
   //gl_FragColor = vec4(initialRay,1.0);
 }
